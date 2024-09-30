ae9e9758863ebbc499919d8dc403090dc7d1da94
Ethan <QustoTemmo@protonmail.com>
2024-09-30 10:08:26 +0100

    Tailwind Colors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,5 +2,5 @@
     import '../app.css';
 </script>
 
-<h1 class="text-4xl text-violet-800 font-semibold">Welcome to SvelteKit</h1>
+<h1 class="text-4xl text-primary-800 font-semibold">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+import colors from 'tailwindcss/colors'
+
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary : colors.indigo,
+        neutral : colors.slate,
+      }
+    },
   },
   plugins: [],
 }
