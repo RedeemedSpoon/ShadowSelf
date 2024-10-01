c3f2adf1b8d09036a0841283fccce966c36f70f9
Ethan <QustoTemmo@protonmail.com>
2024-10-01 09:11:21 +0100

    Little Formatting

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,13 @@
 // import adapter from '@sveltejs/adapter-bun';
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import {vitePreprocess} from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-	kit: {
-		adapter: adapter()
-	}
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter(),
+  },
 };
 
 export default config;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-import colors from 'tailwindcss/colors'
+import colors from 'tailwindcss/colors';
 
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {
       colors: {
-        primary : colors.indigo,
-        neutral : colors.slate,
-      }
+        primary: colors.indigo,
+        neutral: colors.slate,
+      },
     },
   },
   plugins: [],
-}
-
+};
