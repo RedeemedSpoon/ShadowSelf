ed5f6b46fb5325402d3f7e6783a9e41026e4a7b0
Ethan <QustoTemmo@protonmail.com>
2024-11-06 02:55:08 +0100

    Privacy Policy Page

--- Frontend/src/routes/privacy-policy/+page.svelte
+++ Frontend/src/routes/privacy-policy/+page.svelte
@@ -8,73 +8,164 @@
 
 <div>
   <h1>Privacy Policy</h1>
-  <h4>Get to know how we handle your data and privacy.</h4>
+  <h6>Get to know how we handle your data and privacy.</h6>
   <section>
     <p>
-      <span>TLDR</span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia earum adipisci quisquam magnam
-      sit quae? Ab unde, sapiente tempore quas laudantium, nisi error accusantium reiciendis quia illo neque debitis
-      adipisci!
+      <span>TLDR</span> We really value your privacy! We only collect the bare minimum. No tracking, no selling your info.
+      Payments and some other stuff are secure through third-party services. We might share limited info with them to make
+      things work, but that's it. We follow data protection laws and you have rights to your info. We can't guarantee 100%
+      security online, but we try our best. By using our site, you agree to this. We might update this policy, so check back.
     </p>
   </section>
   <p>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur maximus mattis. Vestibulum ante ipsum
-    primis in faucibus orci luctus et ultrices posuere cubilia curae; Etiam non est felis. Pellentesque porttitor justo
-    ac elementum hendrerit. Aliquam feugiat dui quis urna porta tristique. Pellentesque sed tempus ipsum. Vivamus
-    bibendum, turpis vitae fermentum consequat, odio neque blandit velit, at ornare lorem quam non magna. Sed elementum
-    orci neque, id sollicitudin nulla ultrices quis. Cras pharetra dignissim sollicitudin. Cras iaculis consequat metus,
-    condimentum interdum nibh feugiat sit amet. Vestibulum metus nulla, euismod eu finibus eget, finibus id nisl. Nunc
-    in nisl a mi varius scelerisque quis laoreet magna. Sed turpis tellus, condimentum quis imperdiet id, fermentum nec
-    lectus.
+    Shadowself is committed to protecting the privacy of our users. This Privacy Policy outlines the practices we employ
+    regarding the collection, use, disclosure, and protection of user information in connection with our website,
+    located at <a href="https://shadowself.io">shadowself.io</a> (the "Website").
   </p>
+
+  <h3>Information Collection</h3>
   <p>
-    Aenean ac urna at dolor vehicula condimentum ut eget sapien. Pellentesque sodales risus a justo dictum dapibus.
-    Praesent nunc turpis, blandit eget egestas sit amet, pharetra ut nulla. Curabitur lacus metus, tempor nec augue ut,
-    laoreet placerat felis. Proin tellus libero, finibus sed augue eget, efficitur mollis nunc. Cras malesuada viverra
-    ligula, quis ultricies dui gravida ac. In leo diam, congue condimentum odio varius, pellentesque dapibus enim. Cras
-    faucibus arcu at metus lacinia, at ullamcorper dui lobortis. Fusce eleifend ultricies turpis, vitae eleifend mauris.
-    Vestibulum vitae enim mauris. Donec ipsum quam, aliquam sit amet dui ut, ultricies molestie nisi. Pellentesque
-    habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris tempus a augue sit amet
-    consectetur.
+    We adhere to a minimalist data collection approach, gathering only the essential information required for service
+    operation:
   </p>
+  <ul>
+    <li>
+      <b>Account Credentials:</b> Users establish usernames and passwords to create accounts and access functionalities.
+      We store these credentials in an untraceable format, rendering them unlinkable to specific users.
+    </li>
+    <li>
+      <b>Payment Information:</b> Users have the option to utilize cryptocurrency or credit cards for payment purposes. We
+      emphasize that we do not directly store any payment details. Secure third-party payment processors handle all financial
+      transactions.
+    </li>
+    <li>
+      <b>Contact Information:</b> Users may choose to provide their email address for our contact page. We may collect them
+      solely for communication purposes.
+    </li>
+  </ul>
+  <h3>Information Usage</h3>
+  <p>The information we collect serves strictly limited purposes:</p>
+  <ul>
+    <li>
+      <b>Account Management:</b> To facilitate the creation and management of user accounts.
+    </li>
+    <li>
+      <b>Service Delivery:</b> To ensure the seamless delivery of the services you have requested.
+    </li>
+    <li>
+      <b>Communication:</b> To address user inquiries and provide essential service-related updates
+    </li>
+  </ul>
+  <h3>Absence of Spyware and Analytics</h3>
   <p>
-    Nam vitae dui ac dolor sagittis maximus nec mollis ligula. Maecenas sed fermentum odio. Nunc eu massa a risus auctor
-    tempus. Nullam eget interdum diam, vitae malesuada orci. Cras ut maximus turpis, eu iaculis justo. Nulla vehicula
-    erat eget justo vehicula, id rutrum risus ullamcorper. Nulla facilisi. Vestibulum eu tempor sem, at placerat lacus.
+    We prioritize user privacy by refraining from the use of any spyware or analytics tools on the Website. We collect
+    only the explicitly mentioned data and refrain from tracking user activity beyond the essential requirements for
+    functionality.
   </p>
+  <h3>Data Sharing and Disclosure</h3>
   <p>
-    Sed sed molestie mauris. Phasellus tortor dui, dignissim vel elit sed, luctus efficitur sapien. Curabitur nec nunc
-    id quam varius iaculis malesuada congue lorem. Ut pretium dictum nulla, ut suscipit arcu tempor in. Aliquam congue
-    laoreet metus, ut aliquam dui efficitur vitae. Sed a ultrices lectus. Phasellus vel elementum eros. Quisque
-    fringilla quam nec lacus tristique varius. Aenean faucibus nunc eu commodo iaculis. Aenean arcu risus, molestie et
-    accumsan vitae, scelerisque ut nunc. Etiam dignissim nibh finibus dignissim congue. Mauris lacinia sodales dolor id
-    egestas. Nulla hendrerit justo tincidunt sapien consectetur faucibus. Nullam pharetra nisl sed sem facilisis
-    scelerisque. Aliquam feugiat lacus at ullamcorper egestas. Cras finibus leo nibh, at laoreet ante iaculis non. Fusce
-    eu sollicitudin tortor, non tempus quam. Nam iaculis pretium orci, at congue orci suscipit at. Ut laoreet nisi est,
-    rhoncus eleifend metus hendrerit luctus. Pellentesque eros sapien, ultricies a purus in, vestibulum ullamcorper
-    lorem. Vestibulum aliquam tortor non risus accumsan, non dapibus libero imperdiet. Vestibulum ante ipsum primis in
-    faucibus orci luctus et ultrices posuere cubilia curae; Morbi non fringilla odio.
+    We maintain a strict policy of not sharing or disclosing user information with any third parties, with the following
+    limited exceptions:
+  </p>
+  <ul>
+    <li>
+      <b>Essential Third-Party Services:</b> To provide and improve our services, we may share limited user data with carefully
+      selected third-party service providers. These providers are essential for specific functionalities,
+    </li>
+    <li>
+      <b>Third-Party Payment Processors:</b> To facilitate payment transactions, we may share your payment information solely
+      with secure third-party payment processors (Stripe). These processors are contractually bound to uphold the confidentiality
+      and security of your data.
+    </li>
+    <li>
+      <b>Legal Requirements:</b> We may disclose your information if compelled to do so by law or in response to a legitimate
+      legal process, such as a court order or subpoena.
+    </li>
+  </ul>
+  <h3>Data Security</h3>
+  <p>
+    We prioritize the security of user information by implementing robust security measures and industry standards to
+    safeguard against unauthorized access, disclosure, alteration, or destruction. However, it is crucial to acknowledge
+    that no website or internet transmission can guarantee absolute security. We cannot guarantee the complete security
+    of your information.
+  </p>
+  <h3>User Rights</h3>
+  <p>
+    In accordance with applicable data protection laws, you may possess certain rights regarding your information,
+    including:
+  </p>
+  <ul>
+    <li>
+      <b>Right to Access:</b> You have the right to request access and receive a copy of your personal information.
+    </li>
+    <li>
+      <b>Right to Rectification:</b> You have the right to request correction of inaccuracies in your personal information.
+    </li>
+    <li>
+      <b>Right to Erasure:</b> You have the right to request deletion of your personal information.
+    </li>
+  </ul>
+  <h3>Data Protection Law Compliance</h3>
+  <p>
+    We strive to maintain complete compliance with all applicable data protection laws, including the General Data
+    Protection Regulation (GDPR), California Consumer Privacy Act (CCPA), California Privacy Rights Act (CPRA), and
+    California Online Privacy Protection Act (CalOPPA).
+  </p>
+  <h3>Children's Privacy</h3>
+  <p>
+    We have a zero-tolerance policy regarding the collection of information from children under the age of 13. If you
+    are a parent or guardian and believe your child may have provided us with personal information, please contact us
+    immediately. We will take prompt action to remove such information from our systems.
+  </p>
+
+  <h3>Changes to this Privacy Policy</h3>
+  <p>
+    We reserve the right to update this Privacy Policy periodically. We will notify you of any changes by posting the
+    revised Privacy Policy on the Website. We encourage you to periodically review this Privacy Policy for any updates.
+  </p>
+  <h3>Contact Us</h3>
+  <p>
+    If you have any questions or concerns about this Privacy Policy or our data practices, please contact us through our
+    <a href="/contact">contact page</a>.
+  </p>
+  <hr />
+  <p>
+    By using our Website, you acknowledge that you have read and understood this Privacy Policy. Thank you for trusting
+    Shadowself with your privacy.
   </p>
   <small class="text-lg text-neutral-500">Last updated: November 5, 2024</small>
 </div>
 
 <style lang="postcss">
   div {
-    @apply mx-auto my-[10rem] flex h-fit w-1/2 flex-col gap-8 text-neutral-400;
+    @apply mx-auto my-[10rem] flex h-fit w-11/12 flex-col gap-6 text-neutral-400 md:w-3/4 xl:w-1/2;
   }
 
   h1 {
-    @apply basic-style text-6xl;
+    @apply text-5xl text-neutral-300 sm:text-6xl md:text-7xl;
   }
 
-  h4 {
-    @apply -mt-9 text-2xl font-light text-neutral-500;
+  h3 {
+    @apply mt-4 text-3xl text-neutral-300 md:text-5xl;
+  }
+
+  h6 {
+    @apply text-primary-700 -mt-6 text-xl font-light md:mb-4 md:text-2xl;
+  }
+
+  li {
+    @apply list-inside list-disc pl-8;
+
+    & b {
+      @apply font-semibold text-neutral-300;
+    }
   }
 
   section {
     @apply border-primary-700 ml-8 border-l-4 pl-4;
 
     & span {
-      @apply text-primary-700 pr-4 font-bold;
+      @apply text-primary-700 pr-2 font-bold;
     }
 
     & p {
