52da724c4c396353d06e7462ce391a7440115fff
Ethan <QustoTemmo@protonmail.com>
2025-02-25 03:02:32 +0100

    Change Available Location Due To Phone Number Constraints

--- Backend/src/routes/creation-process.ts
+++ Backend/src/routes/creation-process.ts
@@ -89,27 +89,28 @@ export default new Elysia({websocket: {idleTimeout: 300}})
             error = undefined;
           }
 
-          const prompt = `${ethnicity} ${sex} individual, aged ${age}, showcasing authentic and natural features, with realistic skin texture, facial expression, and posture. The person should reflect genuine human traits, with subtle imperfections and a non-stereotypical appearance, background must somewhere in ${lang?.city}, ${lang?.country}, exuding a sense of warmth, personality, and approachability.`;
-
-          const negativePrompt =
-            'hyper-realistic, polished, exaggerated features, overly symmetrical, robotic or artificial facial expressions, cartoonish, stylized, or unrealistic traits, bland one color background';
-
-          const formData = new FormData();
-          formData.append('prompt', prompt);
-          formData.append('aspect_ratio', '1:1');
-          formData.append('output_format', 'webp');
-          formData.append('negative_prompt', negativePrompt);
-
-          const response = await fetch('https://api.stability.ai/v2beta/stable-image/generate/core', {
-            method: 'POST',
-            body: formData,
-            headers: {
-              authorization: `Bearer ${process.env.STABILITY_API_KEY}`,
-              accept: 'image/*',
-            },
-          });
-
-          const picture = await blobToBase64(await response.blob());
+          // const prompt = `${ethnicity} ${sex} individual, aged ${age}, showcasing authentic and natural features, with realistic skin texture, facial expression, and posture. The person should reflect genuine human traits, with subtle imperfections and a non-stereotypical appearance, background must somewhere in ${lang?.city}, ${lang?.country}, exuding a sense of warmth, personality, and approachability.`;
+          //
+          // const negativePrompt =
+          //   'hyper-realistic, polished, exaggerated features, overly symmetrical, robotic or artificial facial expressions, cartoonish, stylized, or unrealistic traits, bland one color background';
+          //
+          // const formData = new FormData();
+          // formData.append('prompt', prompt);
+          // formData.append('aspect_ratio', '1:1');
+          // formData.append('output_format', 'webp');
+          // formData.append('negative_prompt', negativePrompt);
+          //
+          // const response = await fetch('https://api.stability.ai/v2beta/stable-image/generate/core', {
+          //   method: 'POST',
+          //   body: formData,
+          //   headers: {
+          //     authorization: `Bearer ${process.env.STABILITY_API_KEY}`,
+          //     accept: 'image/*',
+          //   },
+          // });
+          //
+          // const picture = await blobToBase64(await response.blob());
+          const picture = await blobToBase64(Bun.file('/home/greed/Code/Bytepicks/static/assets/recommendation.jpg'));
           const identity = {picture, name, bio, sex, age, ethnicity};
           ws.send({identity});
           break;
@@ -136,7 +137,13 @@ export default new Elysia({websocket: {idleTimeout: 300}})
           cookie.set({value: cookie.value + `&&${sanitizedEmail}`});
 
           const location = cookieStore[0];
-          const result = await twilioClient.availablePhoneNumbers(location).local.list({limit: 20});
+          const phoneType = location === 'US' || location === 'CA' ? 'local' : 'mobile';
+
+          const result = await twilioClient.availablePhoneNumbers(location)[phoneType as 'local'].list({
+            limit: 20,
+            smsEnabled: true,
+            excludeAllAddressRequired: true,
+          });
 
           const phone = result.map((phone) => ({phone: phone.phoneNumber, formatted: phone.friendlyName}));
           ws.send({phone});
@@ -184,7 +191,6 @@ export default new Elysia({websocket: {idleTimeout: 300}})
 
           await twilioClient.incomingPhoneNumbers.create({
             emergencyStatus: 'Inactive',
-            voiceUrl: `${origin}/api/voice`,
             smsUrl: `${origin}/api/sms`,
             phoneNumber: phone,
           });
