8f1bb53579aacf0268cbb1f809924a171fe86a49
Ethan <QustoTemmo@protonmail.com>
2025-03-08 14:23:23 +0100

    Redo Repetitive Functions

--- Backend/src/email-imap.ts
+++ Backend/src/email-imap.ts
@@ -1,5 +1,6 @@
 import PostalMime from 'postal-mime';
 import imap from 'imap-simple';
+// @ts-expect-error No declaration file
 import mimelib from 'mimelib';
 
 export async function listenForEmail(ws: WebSocket, user: string, password: string) {
@@ -16,52 +17,22 @@ export async function listenForEmail(ws: WebSocket, user: string, password: stri
       if (mail > 1) return;
 
       await connection.openBox('INBOX');
-
       const messages = await connection.search([`UNSEEN`], {
         bodies: ['HEADER.FIELDS (FROM SUBJECT DATE MESSAGE-ID IN-REPLY-TO REFERENCES)', 'TEXT'],
         struct: true,
       });
 
       for (const message of messages) {
-        const parts = imap.getParts(message.attributes.struct!);
-
-        const rawAttachments = parts.filter(function (part) {
-          return part.disposition && part.disposition.type.toUpperCase() === 'ATTACHMENT';
-        });
-
-        const attachments = await Promise.all(
-          rawAttachments.map((part) =>
-            connection.getPartData(message, part).then((partData) => ({
-              filename: part.disposition.params.filename,
-              data: partData.toString('base64'),
-            })),
-          ),
+        const {messageID, subject, from, date, reference, inReplyTo, attachments, body, type} = await parseMassage(
+          connection,
+          message,
         );
-
-        const details = message.parts[0].body;
-        const rawBody = message.parts[1].body;
-        const type = /<\/?(html|body|head)>/.test(rawBody) ? 'html' : 'text';
-
         await connection.addFlags(message.attributes.uid, ['\\SEEN']);
-
-        const email = await PostalMime.parse(rawBody);
-        const cleanedBody = (email.html || email.text)!.match(/<html[^>]*>(.*?)<\/html>/is);
-        const body = mimelib.decodeQuotedPrintable(cleanedBody?.[0] || email.html || email.text!);
 
         ws.send(
           JSON.stringify({
             type: 'new-email',
-            newEmail: {
-              messageID: details['message-id'][0],
-              subject: details.subject[0],
-              from: details.from[0],
-              date: details.date[0],
-              reference: details.references?.[0],
-              inReplyTo: details['in-reply-to']?.[0],
-              attachments,
-              body,
-              type,
-            },
+            newEmail: {messageID, subject, from, date, reference, inReplyTo, attachments, body, type},
           }),
         );
       }
@@ -97,40 +68,7 @@ export async function fetchEmails(user: string, password: string, from?: number,
   });
 
   for (const message of messages) {
-    const parts = imap.getParts(message.attributes.struct!);
-
-    const rawAttachments = parts.filter(function (part) {
-      return part.disposition && part.disposition.type.toUpperCase() === 'ATTACHMENT';
-    });
-
-    const attachments = await Promise.all(
-      rawAttachments.map((part) =>
-        connection.getPartData(message, part).then((partData) => ({
-          filename: part.disposition.params.filename,
-          data: partData.toString('base64'),
-        })),
-      ),
-    );
-
-    const details = message.parts[0].body;
-    const rawBody = message.parts[1].body;
-    const type = /<\/?(html|body|head)>/.test(rawBody) ? 'html' : 'text';
-
-    const email = await PostalMime.parse(rawBody);
-    const cleanedBody = (email.html || email.text)!.match(/<html[^>]*>(.*?)<\/html>/is);
-    const body = mimelib.decodeQuotedPrintable(cleanedBody?.[0] || email.html || email.text!);
-
-    total.push({
-      messageID: details['message-id'][0],
-      subject: details.subject[0],
-      from: details.from[0],
-      date: details.date[0],
-      reference: details.references?.[0],
-      inReplyTo: details['in-reply-to']?.[0],
-      attachments,
-      body,
-      type,
-    });
+    total.push(await parseMassage(connection, message));
   }
 
   connection.end();
@@ -144,4 +82,41 @@ async function getMessageCount(connection: imap.ImapSimple) {
       else resolve(mes.messages.total);
     });
   });
+}
+
+async function parseMassage(connection: imap.ImapSimple, message: imap.Message) {
+  const parts = imap.getParts(message.attributes.struct!);
+
+  const rawAttachments = parts.filter(function (part) {
+    return part.disposition && part.disposition.type.toUpperCase() === 'ATTACHMENT';
+  });
+
+  const attachments = await Promise.all(
+    rawAttachments.map((part) =>
+      connection.getPartData(message, part).then((partData) => ({
+        filename: part.disposition.params.filename,
+        data: partData.toString('base64'),
+      })),
+    ),
+  );
+
+  const details = message.parts[0].body;
+  const rawBody = message.parts[1].body;
+  const type = /<\/?(html|body|head)>/.test(rawBody) ? 'html' : 'text';
+
+  const email = await PostalMime.parse(rawBody);
+  const cleanedBody = (email.html || email.text)!.match(/<html[^>]*>(.*?)<\/html>/is);
+  const body = mimelib.decodeQuotedPrintable(cleanedBody?.[1] || email.html || email.text!);
+
+  return {
+    messageID: details['message-id'][0],
+    subject: details.subject[0],
+    from: details.from[0],
+    date: details.date[0],
+    reference: details.references?.[0],
+    inReplyTo: details['in-reply-to']?.[0],
+    attachments,
+    body,
+    type,
+  };
 }
