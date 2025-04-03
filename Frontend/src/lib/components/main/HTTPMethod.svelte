64ab218ed517bb9625fdfa0e6a56e4762dec7e1d
Ethan <QustoTemmo@protonmail.com>
2025-04-03 23:44:33 +0100

    HTTPMethod & Section Refrence Part

---
+++ Frontend/src/lib/components/main/HTTPMethod.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import type {Method} from '$type';
+
+  let {method}: {method: Method | undefined} = $props();
+</script>
+
+{#if method}
+  <span class="{method.toLowerCase()} text-nowrap">{method}</span>
+{/if}
+
+<style lang="postcss">
+  span {
+    @apply rounded-full bg-neutral-950 px-2 py-1 text-sm;
+  }
+
+  .get {
+    @apply bg-blue-600 text-blue-100;
+  }
+
+  .post {
+    @apply bg-green-600 text-green-100;
+  }
+
+  .put {
+    @apply bg-yellow-600 text-yellow-100;
+  }
+
+  .patch {
+    @apply bg-orange-600 text-orange-100;
+  }
+
+  .delete {
+    @apply bg-red-600 text-red-100;
+  }
+</style>
--- Frontend/src/routes/docs/+page.svelte
+++ Frontend/src/routes/docs/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-  import {HTTPMethod, CodeExample} from '$component';
   import type {PageData} from './$types';
+  import {HTTPMethod} from '$component';
+  import {formatCasing} from '$format';
 
   let {data}: {data: PageData} = $props();
 </script>
@@ -11,19 +12,71 @@
 </svelte:head>
 
 <div id="docs">
-  <section>
+  <ul class="no-scrollbar sticky top-24 flex h-fit max-h-[calc(100vh-6rem)] flex-shrink-0 flex-col self-start overflow-y-auto pr-12">
     {#each data.docs.sections as section}
-      <ul class="my-2 text-3xl text-neutral-300">{section.title}</ul>
+      <li class="mb-2 mt-6 text-nowrap text-3xl text-neutral-300">{formatCasing(section.title)}</li>
       {#each section.more as subsection}
-        <li class="ml-8 text-xl text-neutral-400">{subsection.title}</li>
+        <li class="my-[8px] ml-9 text-nowrap text-xl text-neutral-400">
+          <HTTPMethod method={subsection.method} />
+          {formatCasing(subsection.title)}
+        </li>
       {/each}
     {/each}
+  </ul>
+  <span class="mr-12 w-px self-stretch bg-neutral-700"></span>
+  <section class="flex w-full flex-col gap-8 self-stretch">
+    <h2 class="mb-4 text-3xl text-neutral-300">Important Title</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus
+      mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum
+      egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad
+      litora torquent per conubia nostra inceptos himenaeos.
+    </p>
+    <p>
+      Donec urna diam, ultrices eleifend quam luctus, posuere efficitur massa. Pellentesque ut nibh faucibus, dignissim odio at,
+      tristique urna. In finibus, mi ac pharetra consequat, mi purus tristique ex, vitae pulvinar metus sem eget augue. Integer eu
+      lacus id eros vestibulum condimentum. Duis mollis posuere viverra. Aenean condimentum, odio quis venenatis auctor, metus mi
+      sollicitudin urna, facilisis euismod ex enim sit amet ipsum. Etiam ultrices felis erat. Morbi ex risus, gravida in nunc quis,
+      egestas iaculis ante.
+    </p>
+    <p>
+      Vivamus dictum tellus non aliquam suscipit. Aliquam velit eros, dictum id tincidunt id, convallis sit amet lorem. Ut ultrices
+      ornare pharetra. Donec iaculis maximus turpis, a feugiat sapien mattis scelerisque. Pellentesque metus neque, feugiat ac euismod
+      vel, viverra vel purus. Ut molestie eleifend nunc quis rhoncus. Proin justo magna, sollicitudin ut arcu quis, imperdiet efficitur
+      lacus. Sed ipsum purus, tempus luctus euismod a, pellentesque vitae turpis.
+    </p>
+    <h2 class="mb-4 text-3xl text-neutral-300">Important Title</h2>
+    <p>
+      Morbi ex sapien, tempor ac luctus vel, tincidunt ac leo. Vestibulum iaculis arcu id eros mollis, ac lacinia lacus tincidunt.
+      Suspendisse consectetur sapien vel malesuada vehicula. Cras rhoncus massa quis mi pellentesque, ac convallis est condimentum.
+      Phasellus lacinia ut ante eu vulputate. Nullam ac odio at purus blandit iaculis. Nunc non tellus eros. Morbi augue leo, mattis ac
+      quam a, iaculis porta ligula. Sed eget iaculis dui. Mauris non pretium justo, eget condimentum quam. Quisque facilisis ligula vel
+      est bibendum, et pulvinar ex egestas. Suspendisse id mollis est. Vivamus lobortis, nibh quis suscipit sagittis, erat arcu
+      hendrerit turpis, nec tempor velit metus in purus. Nulla eget ante ullamcorper, tristique nibh vel, dapibus arcu. Cras a ligula
+      at quam gravida tincidunt.
+    </p>
+    <p>
+      Duis ut quam ipsum. Suspendisse bibendum ac nisi tincidunt tempus. Pellentesque pharetra mollis dui congue mattis. Morbi rutrum
+      enim ac nunc commodo elementum. Vestibulum accumsan ex a rutrum mollis. Quisque magna orci, vulputate malesuada ipsum feugiat,
+      aliquam volutpat nunc. Nunc urna ipsum, laoreet a pharetra ut, faucibus a libero.
+    </p>
+    <p>
+      Vivamus sit amet arcu scelerisque, facilisis ante non, imperdiet arcu. Integer placerat tortor nulla, vitae blandit nulla
+      sollicitudin ac. Sed in quam mauris. Ut dignissim magna ac diam congue consequat. Integer vel lorem sed tellus facilisis
+      tincidunt. Nulla quis dolor porttitor mauris hendrerit malesuada ac a leo. Mauris venenatis ipsum et pulvinar volutpat. Cras vel
+      dolor aliquam, cursus eros ac, mollis libero. Maecenas dictum ornare nisl nec pulvinar. Duis pretium eros augue. Duis sed rhoncus
+      risus.
+    </p>
+    <p>
+      Suspendisse eget tempor massa. Phasellus malesuada ullamcorper finibus. Vivamus eget sodales tortor. Aenean accumsan sapien in
+      purus venenatis pellentesque. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc
+      eget vehicula quam. Phasellus posuere semper lacinia. Curabitur sapien lacus, blandit ac placerat in, eleifend quis justo.
+    </p>
   </section>
-  <section></section>
 </div>
 
 <style lang="postcss">
   #docs {
-    @apply mx-auto my-[15rem] flex min-h-[40vh] w-5/6 flex-col items-center justify-center gap-8 text-neutral-400 md:w-1/2;
+    @apply mx-auto my-[12.5rem] flex w-5/6 text-neutral-400;
   }
 </style>
