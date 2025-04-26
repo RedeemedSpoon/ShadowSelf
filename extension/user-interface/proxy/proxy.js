15db641963d533bd0ed4ac522fb404d7d9104dc1
Ethan <QustoTemmo@protonmail.com>
2025-04-26 05:51:26 +0100

    Background Country Thingy

--- extension/user-interface/proxy/proxy.js
+++ extension/user-interface/proxy/proxy.js
@@ -2,8 +2,11 @@ document.addEventListener('DOMContentLoaded', async () => {
   const params = new URLSearchParams(window.location.search);
   const [id, location, proxy] = params.values();
 
-  const place = location.split(', ')[1] + ', ' + location.split(', ')[2];
+  const [country, ...place] = location.split(', ');
 
+  const url = chrome.runtime.getURL(`assets/countries/${country.toLowerCase()}.svg`);
+
+  document.getElementById('country-background').style.backgroundImage = `url("${url}")`;
   document.getElementById('ip-address').textContent = proxy;
-  document.getElementById('location').textContent = place;
+  document.getElementById('location').textContent = place.join(', ');
 });
