8cd0c60ff15a5965f645c857db3ad3bf154cd2bd
Ethan <QustoTemmo@protonmail.com>
2025-04-26 21:06:43 +0100

    User Interface Proxy Page

--- extension/user-interface/proxy/proxy.js
+++ extension/user-interface/proxy/proxy.js
@@ -3,10 +3,48 @@ document.addEventListener('DOMContentLoaded', async () => {
   const [id, location, proxy] = params.values();
 
   const [country, ...place] = location.split(', ');
-
   const url = chrome.runtime.getURL(`assets/countries/${country.toLowerCase()}.svg`);
 
   document.getElementById('country-background').style.backgroundImage = `url("${url}")`;
-  document.getElementById('ip-address').textContent = proxy;
+  document.getElementById('ip-address').textContent = proxy.split('/')[0];
   document.getElementById('location').textContent = place.join(', ');
+
+  await ConfigureVPN();
 });
+
+async function ConfigureVPN() {
+  const status = document.getElementById('status');
+  const powerBtn = document.getElementById('power');
+  const container = document.getElementById('button-container');
+  const countryContainer = document.getElementById('country');
+
+  status.textContent = 'Disconnected';
+  powerBtn.addEventListener('click', async () => {
+    if (status.textContent.includes('ing')) return;
+
+    powerBtn.classList.add('processing');
+    container.classList.add('processing');
+    countryContainer.classList.add('processing');
+
+    const isConnected = status.textContent === 'Connected';
+    status.textContent = isConnected ? 'Disconnecting' : 'Connecting';
+
+    const interval = setInterval(() => {
+      if (status.textContent.includes('...')) status.textContent = isConnected ? 'Disconnecting' : 'Connecting';
+      else status.textContent += '.';
+    }, 300);
+
+    setTimeout(() => {
+      clearInterval(interval);
+      status.textContent = isConnected ? 'Disconnected' : 'Connected';
+
+      countryContainer.classList.remove('processing');
+      container.classList.remove('processing');
+      powerBtn.classList.remove('processing');
+
+      countryContainer.classList.toggle('active');
+      container.classList.toggle('active');
+      powerBtn.classList.toggle('active');
+    }, 1200);
+  });
+}
