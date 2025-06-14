954bfa105f7ac9ca847918dc4c0bfce88b42a711
Ethan <QustoTemmo@protonmail.com>
2025-06-14 17:30:32 +0100

    Docker Backup Script

---
+++ .scripts/backup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+REMOTE_USER="root"
+REMOTE_HOST="shadowself.io"
+
+DB_USER=$(grep POSTGRES_USER ${SHADOWSELF_PATH}/application/database/.env | cut -d '=' -f2)
+DB_PASSWORD=$(grep POSTGRES_PASSWORD ${SHADOWSELF_PATH}/application/database/.env | cut -d '=' -f2)
+DB_NAME=$(grep POSTGRES_DB ${SHADOWSELF_PATH}/application/database/.env | cut -d '=' -f2)
+
+ssh -i "$SSH_KEY" ${REMOTE_USER}@${REMOTE_HOST} "
+  set -e
+  CONTAINER_ID=\$(docker ps -q --filter 'label=com.docker.compose.project=shadowself-core' --filter 'ancestor=postgres')
+  docker exec -e PGPASSWORD=${DB_PASSWORD} \$CONTAINER_ID \
+    pg_dump -U ${DB_USER} -d ${DB_NAME} | gzip > db_backup.sql.gz
+"
+
+scp "$REMOTE_USER@$REMOTE_HOST:/root/db_backup.sql.gz ."
+ssh "$REMOTE_USER@$REMOTE_HOST" "rm /root/db_backup.sql.gz"
+echo "Database backup completed."
