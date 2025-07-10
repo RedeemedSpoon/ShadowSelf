668ebb4666373d3c44c6bb505e72737fe7a46d0d
Ethan <QustoTemmo@protonmail.com>
2025-07-10 21:22:08 +0100

    Separate Directory for Certbot

---
+++ application/certbot/certbot-entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+trap exit TERM;
+
+until certbot certonly --webroot -w /var/www/certbot --email contact@shadowself.io --agree-tos --no-eff-email --non-interactive \
+  -d shadowself.io \
+  -d www.shadowself.io \
+  -d mail.shadowself.io \
+  -d us.shadowself.io \
+  -d ca.shadowself.io \
+  -d gb.shadowself.io \
+  -d se.shadowself.io; do
+  echo "Certbot failed, retrying in 5 seconds...";
+  sleep 5;
+done
+
+while :; do
+  certbot renew --quiet;
+  sleep 12h & wait ${!};
+done
