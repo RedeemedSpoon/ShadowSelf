1c97797a823bc48e0204e8375bc63d6a3473f6b3
Ethan <QustoTemmo@protonmail.com>
2026-03-03 01:38:56 +0100

    Moving Import Files Around

--- application/frontend/src/lib/icons/index.ts
+++ application/frontend/src/lib/icons/index.ts
@@ -1,88 +1,87 @@
+export {default as CopyIcon} from './actions/Copy.svelte';
+export {default as DownloadIcon} from './actions/Download.svelte';
+export {default as EditIcon} from './actions/Edit.svelte';
+export {default as FilterIcon} from './actions/Filter.svelte';
+export {default as RepeatIcon} from './actions/Repeat.svelte';
+export {default as SearchIcon} from './actions/Search.svelte';
+export {default as SortIcon} from './actions/Sort.svelte';
+export {default as ForwardIcon} from './actions/Forward.svelte';
+export {default as ReplyIcon} from './actions/Reply.svelte';
+export {default as SendIcon} from './actions/Send.svelte';
+export {default as TrashIcon} from './actions/Trash.svelte';
+export {default as FloppyIcon} from './actions/Floppy.svelte';
+
+export {default as GoIcon} from './brands/Go.svelte';
+export {default as CurlIcon} from './brands/Curl.svelte';
+export {default as RustIcon} from './brands/Rust.svelte';
+export {default as PythonIcon} from './brands/Python.svelte';
+export {default as JavascriptIcon} from './brands/Javascript.svelte';
+export {default as BTCIcon} from './brands/BTC.svelte';
+export {default as LTCIcon} from './brands/LTC.svelte';
+export {default as ETHIcon} from './brands/ETH.svelte';
+export {default as XMRIcon} from './brands/XMR.svelte';
+export {default as USDTIcon} from './brands/USDT.svelte';
+
+export {default as AttachmentIcon} from './communication/Attachment.svelte';
+export {default as CommunityIcon} from './communication/Community.svelte';
+export {default as EmailIcon} from './communication/Email.svelte';
+export {default as PhoneIcon} from './communication/Phone.svelte';
+export {default as ProxyIcon} from './communication/Proxy.svelte';
+export {default as WebsocketIcon} from './communication/Websocket.svelte';
+export {default as WWWIcon} from './communication/WWW.svelte';
+export {default as BroadcastIcon} from './communication/Broadcast.svelte';
+
+export {default as DatabaseIcon} from './data/Database.svelte';
+export {default as ActivityIcon} from './data/Activity.svelte';
+export {default as BoltIcon} from './data/Bolt.svelte';
+export {default as ChartIcon} from './data/Chart.svelte';
+export {default as ClockIcon} from './data/Clock.svelte';
+export {default as GasStationIcon} from './data/Gas-Station.svelte';
+export {default as HashIcon} from './data/Hash.svelte';
+export {default as StackIcon} from './data/Stack.svelte';
+export {default as SpreadSheetIcon} from './data/SpreadSheet.svelte';
+
+export {default as CreditCardIcon} from './finance/CreditCard.svelte';
+export {default as WalletIcon} from './finance/Wallet.svelte';
+export {default as MarketIcon} from './finance/Market.svelte';
+export {default as ReceiptIcon} from './finance/Receipt.svelte';
+
+export {default as BackIcon} from './navigation/Back.svelte';
+export {default as IssuesIcon} from './navigation/Issues.svelte';
+export {default as ChangelogIcon} from './navigation/Changelog.svelte';
+export {default as ExternalLinkIcon} from './navigation/ExternalLink.svelte';
+export {default as ChevronIcon} from './navigation/Chevron.svelte';
+export {default as PinIcon} from './navigation/Pin.svelte';
+
+export {default as EyeIcon} from './security/Eye.svelte';
+export {default as KeylockIcon} from './security/Keylock.svelte';
+export {default as KeyIcon} from './security/Key.svelte';
+export {default as LockEditIcon} from './security/LockEdit.svelte';
+export {default as LockRemoveIcon} from './security/LockRemove.svelte';
+export {default as LogoutIcon} from './security/Logout.svelte';
+export {default as RecoveryIcon} from './security/Recovery.svelte';
+
 export {default as SuccessIcon} from './status/Success.svelte';
 export {default as AlertIcon} from './status/Alert.svelte';
 export {default as InfoIcon} from './status/Info.svelte';
-
-export {default as IssuesIcon} from './link/Issues.svelte';
-export {default as ChangelogIcon} from './link/Changelog.svelte';
-export {default as ExternalLinkIcon} from './link/ExternalLink.svelte';
-
-export {default as GoIcon} from './languages/Go.svelte';
-export {default as CurlIcon} from './languages/Curl.svelte';
-export {default as RustIcon} from './languages/Rust.svelte';
-export {default as PythonIcon} from './languages/Python.svelte';
-export {default as JavascriptIcon} from './languages/Javascript.svelte';
-
-export {default as BTCIcon} from './crypto-currencies/BTC.svelte';
-export {default as LTCIcon} from './crypto-currencies/LTC.svelte';
-export {default as ETHIcon} from './crypto-currencies/ETH.svelte';
-export {default as XMRIcon} from './crypto-currencies/XMR.svelte';
-export {default as USDTIcon} from './crypto-currencies/USDT.svelte';
-
-export {default as DownloadIcon} from './action/Download.svelte';
-export {default as SearchIcon} from './action/Search.svelte';
-export {default as FilterIcon} from './action/Filter.svelte';
-export {default as RepeatIcon} from './action/Repeat.svelte';
-export {default as CopyIcon} from './action/Copy.svelte';
-export {default as SortIcon} from './action/Sort.svelte';
-export {default as BackIcon} from './action/Back.svelte';
-export {default as EditIcon} from './action/Edit.svelte';
+export {default as CheckmarkIcon} from './status/Checkmark.svelte';
+export {default as LightBulbIcon} from './status/LightBulb.svelte';
+export {default as QuestionIcon} from './status/Question.svelte';
 
 export {default as UserIcon} from './user/User.svelte';
-export {default as AddUserIcon} from './user/AddUser.svelte';
 export {default as UserAddIcon} from './user/UserAdd.svelte';
+export {default as AddUserIcon} from './user/AddUser.svelte';
 export {default as UserEditIcon} from './user/UserEdit.svelte';
-export {default as MultiUsersIcon} from './user/MultiUsers.svelte';
 export {default as UserDeleteIcon} from './user/UserDelete.svelte';
+export {default as MultiUsersIcon} from './user/MultiUsers.svelte';
+export {default as FemaleIcon} from './user/Female.svelte';
+export {default as MaleIcon} from './user/Male.svelte';
 
-export {default as LockRemoveIcon} from './auth/LockRemove.svelte';
-export {default as RecoveryIcon} from './auth/Recovery.svelte';
-export {default as LockEditIcon} from './auth/LockEdit.svelte';
-export {default as DatabaseIcon} from './auth/Database.svelte';
-export {default as KeylockIcon} from './auth/Keylock.svelte';
-export {default as LogoutIcon} from './auth/Logout.svelte';
-export {default as FemaleIcon} from './auth/Female.svelte';
-export {default as MaleIcon} from './auth/Male.svelte';
-export {default as EyeIcon} from './auth/Eye.svelte';
-export {default as KeyIcon} from './auth/Key.svelte';
-
-export {default as SpreadSheetIcon} from './wallet/SpreadSheet.svelte';
-export {default as ReceiptIcon} from './wallet/Receipt.svelte';
-export {default as ShuffleIcon} from './wallet/Shuffle.svelte';
-export {default as DownArcIcon} from './wallet/DownArc.svelte';
-export {default as FloppyIcon} from './wallet/Floppy.svelte';
-export {default as CameraIcon} from './wallet/Camera.svelte';
-export {default as MarketIcon} from './wallet/Market.svelte';
-export {default as BroomIcon} from './wallet/Broom.svelte';
-export {default as UpArcIcon} from './wallet/UpArc.svelte';
-
-export {default as BoltIcon} from './crypto/Bolt.svelte';
-export {default as HashIcon} from './crypto/Hash.svelte';
-export {default as ClockIcon} from './crypto/Clock.svelte';
-export {default as ChartIcon} from './crypto/Chart.svelte';
-export {default as StackIcon} from './crypto/Stack.svelte';
-export {default as ActivityIcon} from './crypto/Activity.svelte';
-export {default as BroadcastIcon} from './crypto/Broadcast.svelte';
-export {default as GasStationIcon} from './crypto/Gas-Station.svelte';
-
-export {default as WWWIcon} from './communication/WWW.svelte';
-export {default as SendIcon} from './communication/Send.svelte';
-export {default as TrashIcon} from './communication/Trash.svelte';
-export {default as EmailIcon} from './communication/Email.svelte';
-export {default as PhoneIcon} from './communication/Phone.svelte';
-export {default as ReplyIcon} from './communication/Reply.svelte';
-export {default as ProxyIcon} from './communication/Proxy.svelte';
-export {default as WalletIcon} from './communication/Wallet.svelte';
-export {default as ForwardIcon} from './communication/Forward.svelte';
-export {default as WebsocketIcon} from './communication/Websocket.svelte';
-export {default as CommunityIcon} from './communication/Community.svelte';
-export {default as AttachmentIcon} from './communication/Attachment.svelte';
-export {default as CreditCardIcon} from './communication/CreditCard.svelte';
-
-export {default as CheckmarkIcon} from './miscellaneous/Checkmark.svelte';
-export {default as LightBulbIcon} from './miscellaneous/LightBulb.svelte';
-export {default as QuestionIcon} from './miscellaneous/Question.svelte';
-export {default as ChevronIcon} from './miscellaneous/Chevron.svelte';
-export {default as RocketIcon} from './miscellaneous/Rocket.svelte';
-export {default as HappyIcon} from './miscellaneous/Happy.svelte';
-export {default as BookIcon} from './miscellaneous/Book.svelte';
-export {default as PinIcon} from './miscellaneous/Pin.svelte';
+export {default as BookIcon} from './misc/Book.svelte';
+export {default as HappyIcon} from './misc/Happy.svelte';
+export {default as RocketIcon} from './misc/Rocket.svelte';
+export {default as BroomIcon} from './misc/Broom.svelte';
+export {default as CameraIcon} from './misc/Camera.svelte';
+export {default as DownArcIcon} from './misc/DownArc.svelte';
+export {default as UpArcIcon} from './misc/UpArc.svelte';
+export {default as ShuffleIcon} from './misc/Shuffle.svelte';
